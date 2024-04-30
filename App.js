f4fb62178c5cf6e7664f4cead4062aa9d62a52fe
jadjinda <josueadjinda08@gmail.com>
2024-04-30 18:21:50 +0000

    Second commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import AppNavigation from "./App/Navigations/appNavigation";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
-    <AppNavigation/>
+      <AppNavigation/>
   );
 }
--- App/Navigations/appNavigation.js
+++ App/Navigations/appNavigation.js
@@ -5,43 +5,66 @@ import OrderScreen from "../Screens/OrderScreen";
 import NotifScreen from "../Screens/NotifScreen";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import React from "react";
-import { BlurView } from "@react-native-community/blur";
-import { StyleSheet } from "react-native";
-//import { BlurView } from "expo-blur"
+import {Ionicons, Entypo, AntDesign} from "@expo/vector-icons";
+import WelcomeScreen from "../Screens/WelcomeScreen";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function AppNavigation(){
+function HomeNavigation(){
     return(
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: '#b9814e',
+                tabBarInactiveTintColor: 'grey',
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    height: 70,
+                    borderTopLeftRadius: 30,
+                    borderTopRightRadius: 30
+                }
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeScreen}
+                        options={{
+                            tabBarIcon: ({focused, color}) => (
+                                <Entypo name="home" size={24} color={focused? "#b9814e": "grey"} />
+                            ),
+                        }}/>
+            <Tab.Screen name="Fav" component={FavScreen} options={{
+                tabBarIcon: ({focused, color}) => (
+                    <AntDesign name="heart" size={24} color={focused? "#b9814e": "grey"}/>
+                ),
+            }}/>
+            <Tab.Screen name="Order" component={OrderScreen} options={{
+                tabBarIcon: ({focused, color}) => (
+                    <Ionicons name="bag" size={24} color={focused? "#b9814e": "grey"}/>
+                ),
+            }}/>
+            <Tab.Screen name="Notif" component={NotifScreen} options={{
+                tabBarIcon: ({focused, color}) => (
+                    <Ionicons name="notifications" size={24} color={focused? "#b9814e": "grey"}/>
+                ),
+            }}/>
+        </Tab.Navigator>
+    );
+}
+
+function AppNavigation(props){
+    return (
         <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={{
-                    headerShown: false,
-                    tabBarStyle:{
-                        position: "absolute",
-                        borderTopLeftRadius: 20,
-                        borderTopRightRadius: 20,
-                    },
-                    tabBarBackground: () => (
-                        <BlurView
-                            intensity={80}
-                            style={{
-                                ...StyleSheet.absoluteFillObject,
-                                borderTopLeftRadius: 20,
-                                borderTopRightRadius: 20,
-                                overflow: "hidden",
-                                backgroundColor: "transparent",
-                            }}
-                        />
-                    ),
-                }}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Fav" component={FavScreen} />
-                <Tab.Screen name="Order" component={OrderScreen}/>
-                <Tab.Screen name="Notif" component={NotifScreen} />
-            </Tab.Navigator>
+            <Stack.Navigator initialRouteName='Welcome'>
+                <Stack.Screen name="Home" options={{headerShown: false}} component={HomeNavigation}/>
+                <Stack.Screen name="Welcome" options={{headerShown: false}} component={WelcomeScreen}/>
+                <Stack.Screen name="Fav" options={{headerShown: false}} component={FavScreen}/>
+                <Stack.Screen name="Order" options={{headerShown: false}} component={OrderScreen}/>
+                <Stack.Screen name="Notif" options={{headerShown: false}} component={NotifScreen}/>
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
+
+export default AppNavigation;
