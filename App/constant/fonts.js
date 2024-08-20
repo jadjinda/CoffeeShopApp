9325a98f0871af23685afa55057948c7e99d7c3d
jadjinda <josueadjinda08@gmail.com>
2024-08-20 14:04:13 +0000

    20Août commit

---
+++ App/constant/fonts.js
@@ -0,0 +1,12 @@
+export const fonts={
+    Sora:{
+        Bold: require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-Bold.ttf'),
+        ExtraBold : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-ExtraBold.ttf'),
+        ExtraLight : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-ExtraLight.ttf'),
+        Light : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-Light.ttf'),
+        Medium : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-Medium.ttf'),
+        Regular : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-Regular.ttf'),
+        SemiBold : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-SemiBold.ttf'),
+        Thin : require('/home/user/Code/CoffeeShopApp/assets/fonts/Sora-Thin.ttf'),
+    }
+}
